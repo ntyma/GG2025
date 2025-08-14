cc73b9c372ac54f4c27ec0820e2c4453f930b779
SeanLiu8800 <tyson9963@gmail.com>
2025-08-13 23:36:32 -0700

    Improved Detection of Camera Movement Triggers
    -Player shouldn't be able to "sneak" through and manipulate Camera Movement anymore
    -NOTE: Triggers assume that the Backwards Direction is OPPOSITE to the Forward Progress Direction (ie. Right and Left, Up and Down are viable, Right and Down is NOT)

--- Assets/Scripts/CameraScripts/CameraMovementTriggerScript.cs
+++ Assets/Scripts/CameraScripts/CameraMovementTriggerScript.cs
@@ -9,10 +9,11 @@ public class CameraMovementTriggerScript : MonoBehaviour
 
     [SerializeField] private enum Direction
     {
-        Right, Left, Up, Down
+        Right, Left, Up, Down, NoDirection
     }
     [SerializeField] private Direction forwardDirection = Direction.Right;
-    [SerializeField] private Direction backwardDirection = Direction.Left;
+
+    private Direction backwardDirection = Direction.Left;
     private void Start()
     {
         // set backwardDirection to opposite of forwardDirection
@@ -27,30 +28,83 @@ public class CameraMovementTriggerScript : MonoBehaviour
             case Direction.Up:
                 backwardDirection = Direction.Down;
                 break;
-            default:    //case Direction.Down:
+            case Direction.Down:
                 backwardDirection = Direction.Up;
+                break;
+            default:    // case Direction.NoDirection:
+                Debug.Log("Forward Direction for CameraMovementTrigger CANNOT be NoDirection!");
+                forwardDirection = Direction.Right;
+                backwardDirection = Direction.Left;
                 break;
         }
     }
-    //
-    // BUG, Player can manipulate the Camera by not passing through the collider entirely, then leaving from where they came from
-    //
+
+    private Direction playerEnterDirectionX;
+    private Direction playerEnterDirectionY;
+    private Direction playerExitDirectionX;
+    private Direction playerExitDirectionY;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Only consider collisions from Player
         if (collision.gameObject.tag != "Player")
             return;
-
-        if (collision.gameObject.TryGetComponent<PlayerScript>(out PlayerScript playerScript))
+        // Continue if playerRigidBody is acquired
+        if (!collision.gameObject.TryGetComponent<Rigidbody2D>(out Rigidbody2D playerRigidBody))
         {
-            // playerScript.isFacingRight is True when Player progresses Forwards
-            // playerScript.isFacingRight is False when Player progresses Backwards
-            mainCameraScript.UpdateCamera(playerScript.isFacingRight);
+            Debug.Log("playerRigidBody NOT FOUND in CameraMovementTriggerScript!");
+            return;
         }
+        // Check Player's directions when entering Trigger
+        if (playerRigidBody.velocity.x > 0.0f)
+            playerEnterDirectionX = Direction.Right;
+        else if (playerRigidBody.velocity.x < 0.0f)
+            playerEnterDirectionX = Direction.Left;
         else
-            Debug.Log("PlayerScript NOT FOUND!");
+            playerEnterDirectionX = Direction.NoDirection;
+
+        if (playerRigidBody.velocity.y > 0.0f)
+            playerEnterDirectionY = Direction.Up;
+        else if (playerRigidBody.velocity.y < 0.0f)
+            playerEnterDirectionY = Direction.Down;
+        else
+            playerEnterDirectionY = Direction.NoDirection;     
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        // Only consider collisions from Player
+        if (collision.gameObject.tag != "Player")
+            return;
+        // Continue if playerRigidBody is acquired
+        if (!collision.gameObject.TryGetComponent<Rigidbody2D>(out Rigidbody2D playerRigidBody))
+        {
+            Debug.Log("playerRigidBody NOT FOUND in CameraMovementTriggerScript!");
+            return;
+        }
+        // Check Player's direction when exiting Trigger
+        if (playerRigidBody.velocity.x > 0.0f)
+            playerExitDirectionX = Direction.Right;
+        else if (playerRigidBody.velocity.x < 0.0f)
+            playerExitDirectionX = Direction.Left;
+        else
+            playerExitDirectionX = Direction.NoDirection;
+
+        if (playerRigidBody.velocity.y > 0.0f)
+            playerExitDirectionY = Direction.Up;
+        else if (playerRigidBody.velocity.y < 0.0f)
+            playerExitDirectionY = Direction.Down;
+        else
+            playerExitDirectionY = Direction.NoDirection;
+
+        // Update Camera Position
+        if (forwardDirection == Direction.Right || backwardDirection == Direction.Right)
+        {
+            if (playerEnterDirectionX == playerExitDirectionX)
+                mainCameraScript.UpdateCamera(forwardDirection == playerEnterDirectionX);
+        }
+        else if (forwardDirection == Direction.Up || backwardDirection == Direction.Up)
+        {
+            if (playerEnterDirectionY == playerExitDirectionY)
+                mainCameraScript.UpdateCamera(forwardDirection == playerEnterDirectionY);
+        }
     }
 }
