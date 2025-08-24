5054c7e34c68647181146d362e6e058b35abc965
SeanLiu8800 <tyson9963@gmail.com>
2025-08-24 16:28:31 -0700

    Created a Player Spawn/ Death Camera  "Animation"
    -New Camera "Animations" are part of the Player GameObject
    -Reorganized Gameplay Obstacle Prefabs into the GameplayObstacles Folder
    -Updated the Sorting Layer of the Spritemask of the Light Prefab

---
+++ Assets/Scripts/PlayerScripts/PlayerRespawnCameraAnimationScript.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawnCameraAnimationScript : MonoBehaviour
+{
+    [SerializeField] private GameObject playerSpotlightGameObject;
+    [SerializeField] private SpriteMask playerSpotlightSpriteMask;
+    [SerializeField] private SpriteRenderer blackBackgroundSpriteRenderer;
+
+    [SerializeField] private float firstIncrementScale = 1.0f;
+    [SerializeField] private float secondIncrementScale = 7.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetAllSpriteComponents(true);
+        PlayPlayerSpawnCameraAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            PlayPlayerSpawnCameraAnimation();
+        if (Input.GetKeyDown(KeyCode.RightShift))
+            PlayPlayerDeathCameraAnimation();
+    }
+    public void SetAllSpriteComponents (bool Input = true)
+    {
+        playerSpotlightSpriteMask.enabled = Input;
+        blackBackgroundSpriteRenderer.enabled = Input;
+    }
+    public float firstIncrementTime = 0.6f;
+    public float secondIncrementTime = 1.0f;
+    private Coroutine PlayerSpawnAnimationCoroutine;
+    public void PlayPlayerSpawnCameraAnimation(float firstIncrementTime = 0.6f, float secondIncrementTime = 1.0f)
+    {
+        if (PlayerSpawnAnimationCoroutine != null)
+        {
+            StopCoroutine(PlayerSpawnAnimationCoroutine);
+            //SetAllSpriteComponents(false);
+        }
+        else if (PlayerDeathCameraAnimationCoroutine != null)
+        {
+            StopCoroutine(PlayerDeathCameraAnimationCoroutine);
+            //SetAllSpriteComponents(true);
+        }
+
+        this.firstIncrementTime = firstIncrementTime;
+        this.secondIncrementTime = secondIncrementTime;
+        PlayerSpawnAnimationCoroutine = StartCoroutine(PlayPlayerSpawnCameraAnimationCoroutine());
+    }
+    private IEnumerator PlayPlayerSpawnCameraAnimationCoroutine()
+    {
+        //SetAllSpriteComponents(true);
+
+        playerSpotlightGameObject.transform.localScale = Vector3.zero;
+        float Timer = 0.0f;
+        float currentScale = 0.0f;
+        while (Timer <= firstIncrementTime)
+        {
+            yield return null;
+            currentScale = Mathf.Lerp(currentScale, firstIncrementScale, 7.0f * Time.deltaTime);
+            playerSpotlightGameObject.transform.localScale = new Vector3(1.0f, 1.0f, 0.0f) * currentScale;
+            Timer = Timer + Time.deltaTime;
+        }
+        Timer = 0.0f;
+        while (Timer <= secondIncrementTime)
+        {
+            yield return null;
+            currentScale = Mathf.Lerp(currentScale, secondIncrementScale, 2.0f * Time.deltaTime);
+            playerSpotlightGameObject.transform.localScale = new Vector3(1.0f, 1.0f, 0.0f) * currentScale;
+            Timer = Timer + Time.deltaTime;
+        }
+        currentScale = secondIncrementScale;
+        playerSpotlightGameObject.transform.localScale = new Vector3(1.0f, 1.0f, 0.0f) * currentScale;
+
+        //SetAllSpriteComponents(false);
+    }
+
+    public float initialDelay = 0.5f;
+    public float deathFadeoutTime = 1.0f;
+    private Coroutine PlayerDeathCameraAnimationCoroutine;
+    public void PlayPlayerDeathCameraAnimation(float initialDelay = 0.5f, float deathFadeoutTime = 1.0f)
+    {
+        if (PlayerDeathCameraAnimationCoroutine != null)
+        {
+            StopCoroutine(PlayerDeathCameraAnimationCoroutine);
+            //SetAllSpriteComponents(false);
+        }
+        else if (PlayerSpawnAnimationCoroutine != null)
+        {
+            StopCoroutine(PlayerSpawnAnimationCoroutine);
+            //SetAllSpriteComponents(false);
+        }
+
+        this.initialDelay = initialDelay;
+        this.deathFadeoutTime = deathFadeoutTime;
+        PlayerDeathCameraAnimationCoroutine = StartCoroutine(PlayPlayerDeathCameraAnimationCoroutine());
+    }
+    private IEnumerator PlayPlayerDeathCameraAnimationCoroutine()
+    {
+        //SetAllSpriteComponents(true);
+
+        float currentScale = secondIncrementScale;
+        playerSpotlightGameObject.transform.localScale = new Vector3(1.0f, 1.0f, 0.0f) * currentScale;
+        float Timer = 0.0f;
+        while (Timer <= initialDelay)
+        {
+            yield return null;
+            Timer = Timer + Time.deltaTime;
+        }
+        Timer = 0.0f;
+        while (Timer <= deathFadeoutTime)
+        {
+            yield return null;
+            Timer = Timer + Time.deltaTime;
+            currentScale = Mathf.Lerp(currentScale, 0.0f, 7.0f * Time.deltaTime);
+            playerSpotlightGameObject.transform.localScale = new Vector3(1.0f, 1.0f, 0.0f) * currentScale;
+        }
+        playerSpotlightGameObject.transform.localScale = Vector3.zero;
+        //SetAllSpriteComponents(false);
+    }
+}
