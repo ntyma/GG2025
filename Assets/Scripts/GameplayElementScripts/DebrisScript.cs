0b82f5754cce7ea87aafb188a0622bbd5a4d52f4
SeanLiu8800 <tyson9963@gmail.com>
2025-08-30 17:24:51 -0700

    Attched Impact Sounds to Debris
    -I didn't use the falling sound cause the Debris falls/ respawns too fast for it
    -Deleted the Placeholder Background in Mainhouse and Lighthouse Scenes
    -Added Dani's Light component to the Chasing Light Wall
    -Placed some Light Sources in Most Levels

--- Assets/Scripts/GameplayElementScripts/DebrisScript.cs
+++ Assets/Scripts/GameplayElementScripts/DebrisScript.cs
@@ -10,12 +10,13 @@ public class DebrisScript : MonoBehaviourWithReset
     [SerializeField] private int Damage = 5;
     public float debrisSpeed = 2.0f;
 
-    // Reset Component Variable
+    private GameObject playerGameObject;
 
     // Awake is called before the first frame update and before Start
     void Awake()
     {
         respawnPoint = this.transform.position;
+        playerGameObject = GameObject.FindWithTag("Player");
     }
     
     // Update is called once per frame
@@ -28,19 +29,23 @@ public class DebrisScript : MonoBehaviourWithReset
         if (this.transform.position.y <= respawnHeight)
         {
             this.transform.position = respawnPoint;
+            /*if (Mathf.Abs(playerGameObject.transform.position.x - this.transform.position.x) < 5.0f)
+                AudioManager.instance.Play("DebrisFall");*/
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision) // Take damage when colliding with the player
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             var healthComponent = collision.gameObject.GetComponent<Health>();
-            if(healthComponent != null)
+            if (healthComponent != null)
             {
                 healthComponent.TakeDamage(Damage);
             }
         }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
+            AudioManager.instance.Play("DebrisImpact");
     }
     public override void ResetToInstantiation()
     {
