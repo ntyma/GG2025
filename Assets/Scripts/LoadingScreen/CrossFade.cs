2ae11952fa5b6e644aa0c62aa262e2ffbad2d7f0
Sophia Don Tranho <syd3@sfu.ca>
2025-08-31 22:06:33 -0700

    2s fake loading

--- Assets/Scripts/LoadingScreen/CrossFade.cs
+++ Assets/Scripts/LoadingScreen/CrossFade.cs
@@ -9,13 +9,13 @@ public class CrossFade : SceneTransition
 
     public override IEnumerator AnimateTransitionIn()
     {
-        var tweener = crossFade.DOFade(1f, 1f);
+        var tweener = crossFade.DOFade(1f, 0.75f); // fade in over 0.75 seconds
         yield return tweener.WaitForCompletion();
     }
 
     public override IEnumerator AnimateTransitionOut()
     {
-        var tweener = crossFade.DOFade(0f, 1f);
+        var tweener = crossFade.DOFade(0f, 1f); // fade out over 1 seconds
         yield return tweener.WaitForCompletion();
     }
 }
