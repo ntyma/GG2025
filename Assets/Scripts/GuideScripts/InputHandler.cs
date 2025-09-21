b4e06953b7d1061037a7e119a429df3fb640baac
SeanLiu8800 <tyson9963@gmail.com>
2025-09-20 19:22:17 -0700

    Sync Guide Light to Scroll Wheel
    -This required the InputHandler to have a reference to the Light2D script of course, so I made a reference between them it in both scenes as well

--- Assets/Scripts/GuideScripts/InputHandler.cs
+++ Assets/Scripts/GuideScripts/InputHandler.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 public class InputHandler : MonoBehaviour
 {
     private Camera _mainCamera;
 
     [SerializeField] private SpriteMask spriteMask;
-
+    [SerializeField] private Light2D light2DScript;
     private float targetScale;
 
     [SerializeField] private float scrollSensitivity = 0.001f;
@@ -16,6 +17,15 @@ public class InputHandler : MonoBehaviour
     [SerializeField] private float maxSize = 2f;
     [SerializeField] private float lerpSpeed = 10f;
 
+    // For 2D light
+    [SerializeField] private float lightInnerRadius = 0.54f;
+    [SerializeField] private float lightOuterRadius = 1.93f;
+    private float lightOuterRadiusTarget;
+
+    // Variable to keep track of current Scroll Progress as a percentage
+    // This is so I can use Lerp with this as the interpolation percentage
+    // for both SpriteMask Scale and the Light Radius
+    private float scrollProgress = 1.0f;
     private void Awake()
     {
         _mainCamera = Camera.main;
@@ -23,6 +33,11 @@ public class InputHandler : MonoBehaviour
         if (spriteMask != null)
         {
             targetScale = spriteMask.transform.localScale.x;
+        }
+
+        if (light2DScript != null)
+        {
+            lightOuterRadiusTarget = light2DScript.pointLightOuterRadius;
         }
     }
 
@@ -35,6 +50,14 @@ public class InputHandler : MonoBehaviour
 
             spriteMask.transform.localScale = new Vector3(newScale, newScale, 1f);
         }
+        // Light Radius Adjustment
+        if (light2DScript != null)
+        {
+            float currentOuterRadius = light2DScript.pointLightOuterRadius;
+            float newOuterRadius = Mathf.Lerp(currentOuterRadius, lightOuterRadiusTarget, Time.deltaTime * lerpSpeed);
+
+            light2DScript.pointLightOuterRadius = newOuterRadius;
+        }
     }
 
     public void OnScroll(InputAction.CallbackContext context)
@@ -44,10 +67,17 @@ public class InputHandler : MonoBehaviour
         if (Mathf.Abs(scrollY) > 0.01f)
         {
             // accumulate target size instead of snapping
-            targetScale += scrollY * scrollSensitivity;
+            //targetScale += scrollY * scrollSensitivity;
 
-            // clamp so it doesn't grow/shrink forever
-            targetScale = Mathf.Clamp(targetScale, minSize, maxSize);
+            // update scrolProgress percentage so I can determine Target Sizes between maxSize and minSize
+            scrollProgress = Mathf.Clamp(scrollProgress + (scrollY * scrollSensitivity), 0.0f, 1.0f);
+            targetScale = Mathf.Lerp(minSize, maxSize, scrollProgress);
+
+            // Don't need below because targetScale is guaranteed to be between minSize and maxSize
+                        // clamp so it doesn't grow/shrink forever
+                        //targetScale = Mathf.Clamp(targetScale, minSize, maxSize);
+
+            lightOuterRadiusTarget = Mathf.Lerp(lightInnerRadius, lightOuterRadius, scrollProgress);
         }
     }
 }
